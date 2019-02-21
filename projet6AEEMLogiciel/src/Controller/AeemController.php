79456cf6e63cfe37102015c08f67569de12e9601
Fabien Araneder--Ursuegui <faursuegui@iutbayonne.univ-pau.fr>
2019-02-21 12:28:40 +0100

    Ajout Bootstrap et Mise en Place page Accueil

--- projet6AEEMLogiciel/src/Controller/AeemController.php
+++ projet6AEEMLogiciel/src/Controller/AeemController.php
@@ -8,12 +8,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class AeemController extends AbstractController
 {
     /**
-     * @Route("/aeem", name="aeem")
+     * @Route("/", name="Accueil")
      */
     public function index()
     {
-        return $this->render('aeem/index.html.twig', [
-            'controller_name' => 'AeemController',
-        ]);
+        return $this->render('aeem/index.html.twig');
+    }
+
+     /**
+     * @Route("/GestionEleves", name="GestionEleves")
+     */
+    public function GestionEleves()
+    {
+        return $this->render('aeem/GestionEleves.html.twig');
+    }
+
+     /**
+     * @Route("/GesionProfesseurs", name="GesionProfesseurs")
+     */
+    public function GesionProfesseurs()
+    {
+        return $this->render('aeem/GesionProfesseurs.html.twig');
+    }
+
+     /**
+     * @Route("/GestionEtablissements", name="GestionEtablissements")
+     */
+    public function GestionEtablissements()
+    {
+        return $this->render('aeem/GestionEtablissements.html.twig');
+    }
+
+     /**
+     * @Route("/GestionSubventions", name="GestionSubventions")
+     */
+    public function GestionSubventions()
+    {
+        return $this->render('aeem/GestionSubventions.html.twig');
     }
 }
