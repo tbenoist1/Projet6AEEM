133e76b9e553b3dcb31b5e682703c38e83163751
Pierre Gourinel <pgourinel@iutbayonne.univ-pau.fr>
2019-03-24 21:20:51 +0100

    un bon début

--- projet6AEEMLogiciel/src/Form/EleveType.php
+++ projet6AEEMLogiciel/src/Form/EleveType.php
@@ -6,6 +6,8 @@ use App\Entity\Eleve;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class EleveType extends AbstractType
 {
@@ -14,8 +16,11 @@ class EleveType extends AbstractType
         $builder
             ->add('nom')
             ->add('prenom')
-            ->add('sexe')
-            ->add('dateNaissance')
+            ->add('sexe' ,ChoiceType::class, ['choices' => ['G' => 1,
+                                                            'F' => 2],
+                                                'expanded' => true,
+                                                'multiple' => false])
+            ->add('dateNaissance' ,DateType::class ,['widget' => 'single_text'])
             ->add('anneeSuivie')
             ->add('adresse')
             ->add('codePostal')
@@ -23,20 +28,27 @@ class EleveType extends AbstractType
             ->add('courriel')
             ->add('telephoneO')
             ->add('telephoneS')
-            ->add('niveau')
+            ->add('niveau', ChoiceType::class, ['choices' => ['primaire' => 1,
+                                                                'collège' => 2,
+                                                                'lycée' => 3],
+                                                'expanded' => false,
+                                                'multiple' => false])
             ->add('classe')
             ->add('dureeIntervention')
             ->add('lieuIntervention')
             ->add('contact')
             ->add('contactNum')
-            ->add('dateDebut')
-            ->add('dateFin')
-            ->add('certificatMedical')
-            ->add('ri')
-            ->add('enveloppes')
-            ->add('cheques')
+            ->add('dateDebut' ,DateType::class ,['widget' => 'single_text'])
+            ->add('dateFin' ,DateType::class ,['widget' => 'single_text'])
+            ->add('certificatMedical', ChoiceType::class, ['choices' => ['certificatMedical' => 1,
+                                                                'ri' => 2,
+                                                                'enveloppes' => 3,
+                                                                'chèques' => 4],
+
+                                                'expanded' => false,
+                                                'multiple' => false])
             //->add('professeurs')
-            //->add('etablissement')
+            //->add('etablissement' ,EntityType::class, array('class' => Etablissement::class, 'choice_label' => 'nom', 'multiple' => false, 'expanded' => false))
         ;
     }
 
