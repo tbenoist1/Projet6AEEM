3f679232c3008a5a79bad3459d53849235ca604e
Timothee Benoist <tbenoist@iutbayonne.univ-pau.fr>
2019-03-28 09:50:09 +0100

    ajouterProfesseur + couleur survol

--- projet6AEEMLogiciel/src/Form/ProfesseurType.php
+++ projet6AEEMLogiciel/src/Form/ProfesseurType.php
@@ -6,6 +6,7 @@ use App\Entity\Professeur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ProfesseurType extends AbstractType
 {
@@ -22,15 +23,34 @@ class ProfesseurType extends AbstractType
             ->add('telephoneS')
             ->add('situationActuelle')
             ->add('matieres')
-            ->add('niveau')
-            ->add('zonesInterventions')
-            ->add('lieuxInterventions')
-            ->add('toutesMaladies')
-            ->add('voiture')
+            ->add('niveau', ChoiceType::class, ['choices' => ['Primaire' => 1,
+                                                              'Collège' => 2,
+                                                              'Lycée' => 3],
+                                                'expanded' => false,
+                                                'multiple' => false])
+            ->add('zonesInterventions', ChoiceType::class, ['choices' => ['BAB' => 1,
+                                                            'Pays Basque Intérieur' => 2,
+                                                            'Sud Pays Basque' => 3,
+                                                            'Sud Landes' => 4],
+                                                'expanded' => true,
+                                                'multiple' => true])
+            ->add('lieuxInterventions', ChoiceType::class, ['choices' => ['Domicile' => 1,
+                                                                      'Hôpital' => 2],
+                                                'expanded' => true,
+                                                'multiple' => true])
+            ->add('toutesMaladies', ChoiceType::class, ['choices' => ['Oui' => 1,
+                                                                      'Non' => 2],
+                                                'expanded' => true,
+                                                'multiple' => false])
+            ->add('voiture', ChoiceType::class, ['choices' => ['Oui' => 1,
+                                                               'Non' => 2],
+                                                'expanded' => true,
+                                                'multiple' => false])
             ->add('cv')
             ->add('cj')
             ->add('commentaires')
             //->add('eleves')
+
         ;
     }
 
